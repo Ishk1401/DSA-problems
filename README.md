1c8349e4335d4403f252ce8629dfc6c551404ac3
Ishk1401 <ishk1401@gmail.com>
2025-08-24 23:17:18 +0530

    Initial commit: setup DSA problems repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# DSA-probems-
+solutions of DSA problems
+
+In this repository i upload my pratice of DSA problems all type of problems that i solve 
---
+++ selection_sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[] , int n ) {
+    for( int i = 0 ; i < n ; i++){
+        int mini= i;
+        for(int j= i; j < n ; j++){
+            if( arr[j] < arr[mini] ) mini = j;
+        }
+        int temp = arr[mini];
+        arr[mini]= arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    //loop for taking inputs in array , index vise//
+    for(int i = 0 ; i<n ; i++) cin>> arr[i];
+    selection_sort(arr, n);
+
+    // loop for producing the output 
+    for(int i = 0 ; i < n ; i++) cout << arr[i] << " ";
+    
+    return 0; 
+}
